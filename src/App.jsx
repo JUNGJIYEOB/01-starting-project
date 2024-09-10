92b466be7b942260df7d5e59f16f3837bb17c09e
wjdwlduq1 <wjdwwuq1@gmail.com>
2024-09-10 16:54:51 +0900

    주석 정리

--- src/App.jsx
+++ src/App.jsx
@@ -82,13 +82,13 @@ function App() {
             <TabButton onSelect={() => handleClick("state")}>State</TabButton>
           </menu>
           {/* 
-            1.조건부 랜더링-1삼항연산자_null 사용 
+            1.조건부 랜더링
+              1) 삼항연산자_null 사용 
             null : JSX 코드로 출력할 수 있으며 단순히 아무것도 렌더링 되지 않는 것
             {!selectedTopic?(<p>Please selct a topic</p>):null}
             {selectedTopic?(
             <div id='tab-content'>
               <h3>{EXAMPLES[selectedTopic].title}</h3>
-                
               <p>{EXAMPLES[selectedTopic].description}</p>
               <pre>
                 <code>
@@ -99,7 +99,8 @@ function App() {
           ) :null}
         */}
           {/**
-         * 1.조건부 랜더링-2 삼항연산자
+         * 1.조건부 랜더링
+         *  2) 삼항연산자
         {!selectedTopic?(<p>Please selct a topic</p>)
         :( 
         <div id='tab-content'>
@@ -114,7 +115,8 @@ function App() {
         )}
          */}
           {/*
-          * 1.조건부 랜더링-3 && 연산자
+          * 1.조건부 랜더링-
+              3) && 연산자
           * true && expression은 항상 expression으로 평가되고 false && expression은 항상 false로 평가
           {!selectedTopic&&(<p>Please selct a topic</p>)}
           {selectedTopic&&(
@@ -130,7 +132,9 @@ function App() {
             </div>
           ) }
          */}
-          {/*1.조건부 랜더링_컴포넌트 내부에서 if문 내부에서 변수에 재할당 */}
+          {/*1.조건부 랜더링
+           * 3)컴포넌트 내부에서 if문 내부에서 변수에 재할당
+           */}
           {tabContents}
         </section>
         <section>
