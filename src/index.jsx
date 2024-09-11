15a40e60b54dd9cf3a245a559a3b82f82f8e8a83
wjdwlduq1 <wjdwwuq1@gmail.com>
2024-09-11 14:30:12 +0900

    jsx 미사용

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,9 @@
-import ReactDOM from "react-dom/client";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 
-import App from "./App.jsx";
-import "./index.css";
+import App from './App.jsx';
+import './index.css';
 
-const entryPoint = document.getElementById("root");
+const entryPoint = document.getElementById('root');
 ReactDOM.createRoot(entryPoint).render(<App />);
+//ReactDOM.createRoot(entryPoint).render(React.createElement(App)); -> jsx 미사용 but 알고는 있어야 함
